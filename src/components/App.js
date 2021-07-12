af30adca96b8027da209a391dffa40f6bb4b96d8
Christina Ma <74700240+steen4reals@users.noreply.github.com>
2021-07-12 17:39:50 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Create from './Create'
 
 
 
 function App() {
-	const [ randomQuote, setRandomQuote ] = useState('');
+	const [ randomQuote, setRandomQuote ] = useState('It’s not a bug — it’s an undocumented feature.');
 
 	const[ max, setMax ] = useState(6);
 
@@ -13,16 +13,16 @@ function App() {
 		return Math.floor(Math.random() * max);
 	  }
 
-	useEffect(()=>{
-		getQuote();
-	}, []);
-
 	async function getQuote() {
-			const res = await fetch(process.env.REACT_APP_RANDOM_API_URL);
-			const data = await res.json();
-			setRandomQuote(data[randNum(max)].wisdom);
-			setMax(data.length);
-		}
+		const res = await fetch(process.env.REACT_APP_RANDOM_API_URL);
+		const data = await res.json();
+		setRandomQuote(data[randNum(max)].wisdom);
+		setMax(data.length);
+	}
+
+	// useEffect(()=>{
+	// 	getQuote();
+	// },[]);
 
 	function handleClick() {
 		getQuote();
